0740bee6b00a80c2f495b4891dc584df5c98ddf5
Sky Xu <skyrocks88xmz@gmail.com>
2017-01-29 20:08:18 -0500

    Final project

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 $(document).ready(function() {
   console.log('app.js linked');
-
-  var $startButton = $('#start');
+  var $maincircle = $('.Maincircle');
+  var $easyButton = $('#Easy');
+  var $hardButton = $('#Hard');
   var $scoreInput = $('input');
   var $redButton = $('#red');
   var $greenButton = $('#green');
@@ -13,14 +14,45 @@ $(document).ready(function() {
   var buttonArray = [$yellowButton,$blueButton,$redButton,$greenButton];
   var currentSequence = [$yellowButton,$blueButton,$redButton,$greenButton];
   var counter = 0;
-
-  // $startButton.on('click',function()
-  //    f
-  // })
-
+  var hardMode = false;
   $quitButton.on('click',function(){
       alert('Game Over');
-    })
+      location.reload(true);
+  })
+
+  function beepRed() {
+    var sound = $('#beep1')[0];
+    sound.play();
+  }
+
+  function beepBlue() {
+    var sound = $('#beep2')[0];
+    sound.play()
+  }
+
+  function beepGreen() {
+    var sound = $('#beep3')[0];
+    sound.play();
+  }
+
+  function beepYellow() {
+    var sound = $('#beep4')[0];
+    sound.play();
+  }
+
+  $redButton.on('click', function(){
+     beepRed();
+  })
+  $blueButton.on('click', function(){
+     beepBlue();
+  })
+  $greenButton.on('click', function(){
+     beepGreen();
+  })
+  $yellowButton.on('click', function(){
+     beepYellow();
+  })
+
 
   var proceed = function(){
       addColor = buttonArray[Math.floor(Math.random()*4)];
@@ -36,20 +68,46 @@ var flashSequence = function(){
       //alert user for turn
       //turn on event listeners
     } else {
-      currentSequence[counter].addClass('flashcolor')
+      currentSequence[counter].addClass('flashcolor');
+        if(currentSequence[counter] === $redButton){
+        beepRed();
+        } else if (currentSequence[counter] === $blueButton){
+        beepBlue();
+        } else if (currentSequence[counter] === $greenButton){
+        beepGreen();
+        } else if (currentSequence[counter] === $yellowButton){
+        beepYellow();
+        };
+      console.log('beepfunction',counter);
       var changeBack = function(count){
         // console.log("ITERATION");
         currentSequence[count].removeClass('flashcolor');
       }
       setTimeout(changeBack.bind(null, counter), 900);
       counter++;
+       console.log('flashfunction',counter);
     }
   }
   var timer = setInterval(function(){
-  changeButton() },1000);
+    changeButton() },1000);
 }
 
-flashSequence();
+  $easyButton.on('click', function(){
+    // location.reload(true);
+    // $(this).addClass('easytrigger');
+    // $hardButton.removeClass('hardtrigger');
+    flashSequence();
+     $maincircle.removeClass('spinning');
+
+  })
+
+  $hardButton.on('click', function(){
+    // location.reload(true);
+    // $(this).addClass('hardtrigger');
+    // $easyButton.removeClass('easytrigger');
+    hardMode();
+    flashSequence();
+  })
 
 $circleButton.on('click',function() {
   if(currentSequence[counter].attr('id') === $(this).attr('id')) {
@@ -65,12 +123,32 @@ $circleButton.on('click',function() {
       // console.log('you got this round right!');
       // increase seq
       proceed();
-       $scoreInput.val(parseInt($scoreInput.val()) + 1);
+      $scoreInput.val(parseInt($scoreInput.val()) + 1);
+      // setTimeout(function(){
+      // $maincircle.css('background-image','url("http://twinpossible.com/wp-content/uploads/2011/11/thumbs-up-smiley-300x182.gif")');
+      // },500);
     }
   } else {
      $scoreInput.val('0');
-    alert('you lose');
+     alert('you lose');
+     $maincircle.removeClass('spinning');
   }
 })
+
+var hardMode = function(){
+  $maincircle.addClass('spinning');
+}
+
+
+
+
+
+
+
+
+
+
+
+
 
 });
---
+++ readme.md
@@ -0,0 +1,35 @@
+1.Explanation of technologies used:
+ I used Jquery for the falsh, beep, click, switch mode and buttons function. 
+ And css, html for the game layout.
+
+2.Approach taken:
+  The flashSequence function is for the simon game to flash and beep in random rotation order.
+  The order of the color buttons clicked is totally random, cause I pushed different colors from
+  an array of four colors,which makes the chance of colors picked to be 25% each.
+  I used on circlebuttons on click to make the buttons flash and beep whn you click the right one,
+  if you clicked the wrong one, the alert window would pop out and tell you that you lost.
+  Also the game has two mode, one with the circle being still, one with the circle go rolling
+  in rotation. 
+ 
+3.User stories:
+ -As a user, I would be able to play the lovely vintage simon game. It's a game that can
+ improve your memory by practicing more!
+ -I can switch the game mode to a harder level, so that you'll never be bored.
+ -I can see the buttons flash in random order at the same time hear the different beep sound to different colors, and click the buttons also hear the beep sound .
+ -I can quit the game any time I want.
+ -I get to see the score I got.
+
+4.Wireframe:
+Header(the name of the game)
+nav bar(with hidden bar feature to switch to different mode)
+the main part(the game circle with buttons)
+footer(score and quit button)
+    
+5.How to use instructions
+First:hover the mouse over the "Select your game mode" bar, see the easy mode and hard mode below.
+Second:choose the mode you want and enjoy the game.
+Third:click the colored buttons following the flashed and beeped order.
+Fourth:quit the game if you want to end, or want to switch to different mode.
+
+6.Unsolved problems
+I didn't figure out how to make the thumbs up image to pop out in the center of the page when you clicked the right order and stop the game temporarily as the image poped out every time and then disappear the image .
